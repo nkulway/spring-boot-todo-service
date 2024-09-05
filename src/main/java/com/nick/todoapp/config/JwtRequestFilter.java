11b0e5489da1a29e74107a117b7ddbda476e9584
“Nick” <nkulway@gmail.com>
2024-09-04 23:55:36 -0400

    jwt req filter

---
+++ src/main/java/com/nick/todoapp/config/JwtRequestFilter.java
@@ -0,0 +1,30 @@
+package com.nick.todoapp.config;
+
+import com.nick.todoapp.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+    }
+
+
+}
