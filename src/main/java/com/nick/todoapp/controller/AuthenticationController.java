cb3d9547ea4dee653475ad27f930423f064e142d
“Nick” <nkulway@gmail.com>
2024-09-09 22:46:49 -0400

    private members

--- src/main/java/com/nick/todoapp/controller/AuthenticationController.java
+++ src/main/java/com/nick/todoapp/controller/AuthenticationController.java
@@ -1,7 +1,15 @@
 package com.nick.todoapp.controller;
 
+import com.nick.todoapp.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AuthenticationController {}
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationController authenticationController;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+}
