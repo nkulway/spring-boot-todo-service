8917e207b7e15417e1b86038be5246d0e899ef25
“Nick” <nkulway@gmail.com>
2024-09-09 22:56:34 -0400

    add auth logic

--- src/main/java/com/nick/todoapp/controller/AuthenticationController.java
+++ src/main/java/com/nick/todoapp/controller/AuthenticationController.java
@@ -1,15 +1,41 @@
 package com.nick.todoapp.controller;
 
+import com.nick.todoapp.dto.JwtRequest;
+import com.nick.todoapp.dto.JwtResponse;
 import com.nick.todoapp.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @RestController
 public class AuthenticationController {
 
     @Autowired
-    private AuthenticationController authenticationController;
+    private AuthenticationManager authenticationManager;
 
     @Autowired
     private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostMapping("/authenticate")
+    public JwtResponse createAuthenticationToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword())
+            );
+        } catch (Exception e) {
+            throw new Exception("Invalid credentials", e);
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(jwtRequest.getUsername());
+        final String token = jwtUtil.generateToken(userDetails.getUsername());
+
+        return new JwtResponse(token);
+    }
 }
