804a46ac744b33ec5be56310621723ab7aea63b8
Nick Kulway <75750859+nkulway@users.noreply.github.com>
2024-09-10 22:35:58 -0400

    security filter (#4)
    * new class
    
    * private members
    
    * add auth logic
    
    * security filter chain
    
    ---------
    
    Co-authored-by: “Nick” <“nkulway@gmail.com”>

--- src/main/java/com/nick/todoapp/config/SecurityConfig.java
+++ src/main/java/com/nick/todoapp/config/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.nick.todoapp.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()).httpBasic(withDefaults());
+        return http.build();
+    }
 }
